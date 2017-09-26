0ff77f405da190f7e7dbf7253cf2afb74b421c38
komalheer <komalheer1992@gmail.com>
2017-09-26 10:13:41 -0700

    Delete async.js

--- LEARNYOUNODE/async.js
+++
@@ -1,8 +0,0 @@
-var fs=require('fs') ;
-fs.readFile(process.argv[2],'utf8',function(e,call){
-if (e) {
-return console.log(e);
-} 
-const ans = call.split('\n').length-1;
-console.log(ans);    
-});
